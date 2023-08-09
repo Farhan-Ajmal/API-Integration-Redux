b4b7701f2567b3d8b2813271667613b554b9a388
Farhan Ajmal <farhanajmal789@gmail.com>
2023-08-09 06:13:36 -0700

    all images added, created vehicle page

---
+++ src/allImages/index.jsx
@@ -0,0 +1,15 @@
+import car from "../assets/icon/car.svg"
+import carLeft from "../assets/icon/car-L.png"
+import carRight from "../assets/icon/car-R.png"
+import truck from "../assets/icon/truck.svg"
+import defaultIcon from "../assets/icon/complaints.webp"
+
+const AllImages = {
+    car: car,
+    carLeft: carLeft,
+    carRight: carRight,
+    truck: truck,
+    default: defaultIcon
+}
+
+export default AllImages
